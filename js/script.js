cc308c1ec7a8f63e8c386106e49e75fa6be11688
Trippnology <trippnology@gmail.com>
2013-09-21 22:24:48 +0100

    Tweaks for bs3 and responsive classes.

--- js/script.js
+++ js/script.js
@@ -22,7 +22,7 @@ jQuery(document).ready(function($) {
 			var weather = $.xml2json(xml);
 
 			$('<img>').attr({src: weather.image.url, alt: 'Weather Underground logo', class: 'pull-right'}).prependTo('.station');
-			$('<ul class="station-info unstyled"></ul>').appendTo('.station');
+			$('<ul class="station-info list-unstyled"></ul>').appendTo('.station');
 			$('<li>').html('<a href="'+weather.history_url+'" target="blank">'+weather.station_id+'</a>').addClass('station_id').appendTo('.station-info');
 			// TODO Map link needs improving.
 			$('<li>').html(weather.location.full+' ').addClass('location').appendTo('.station-info');
@@ -32,7 +32,7 @@ jQuery(document).ready(function($) {
 									.html('<i class="icon-map-marker"></i>').appendTo('.location');
 			$('<li>').text(weather.station_type).addClass('station_type').appendTo('.station-info');
 
-			$('<ul class="current-observation unstyled"></ul>').appendTo('.current-conditions');
+			$('<ul class="current-observation list-unstyled"></ul>').appendTo('.current-conditions');
 			$('<li>').text(weather.observation_time_rfc822).appendTo('.current-observation');
 			$('<li>').text(weather.temperature_string).addClass('badge').appendTo('.current-observation');
 			$('<li>').text(weather.pressure_string).addClass('badge').appendTo('.current-observation');
